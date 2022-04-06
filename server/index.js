38dba16d72a205251f549d442610eb193abc317f
coding-to-music <connors.tom@gmail.com>
2022-04-06 04:29:53 +0000

    Heroku

--- server/index.js
+++ server/index.js
@@ -1,12 +1,17 @@
 // server.js
 // where your node app starts
 
+const path = require("path");
+
 // init project
 const express = require("express");
 const app = express();
 
 // we've started you off with Express,
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
+
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, "/public")));
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("dist"));
