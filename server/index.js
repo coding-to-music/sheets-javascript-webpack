8dae739f5461358af652d8a25a855a7765bfde6f
coding-to-music <connors.tom@gmail.com>
2022-04-06 04:16:48 +0000

    Heroku

--- server/index.js
+++ server/index.js
@@ -18,5 +18,9 @@ app.get("/", (request, response) => {
 
 // listen for requests :)
 const listener = app.listen(process.env.PORT, () => {
-  console.log(`Your app is listening on port ${listener.address().port}`);
+  console.log(
+    `Your app is listening on port ${
+      listener.address().port
+    } or on http://localhost:${listener.address().port}`
+  );
 });
